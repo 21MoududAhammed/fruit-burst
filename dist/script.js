d2ed90cf5496bdc71d86f6a4cf34a968dbf91938
Moudud <21moudud@gmail.com>
2023-12-12 11:32:04 +0600

    Responsive navbar has done

--- dist/script.js
+++ dist/script.js
@@ -1,15 +1,19 @@
 function toggleBtn(e){
-    const navLinks = document.getElementById('nav-links')
+    const navLinks = document.getElementById('nav-links');
+    const searchBox = document.getElementById('search-box');
+    console.log(searchBox)
     console.log(navLinks)
     if(e.classList.contains('fa-bars')){
         e.classList.remove('fa-bars');
         e.classList.add('fa-xmark');
         navLinks.classList.remove('hidden');
+        searchBox.classList.remove('hidden');
 
     }
     else{
         e.classList.remove('fa-xmark');
         e.classList.add('fa-bars');
         navLinks.classList.add('hidden');
+        searchBox.classList.add('hidden');
     }
 }
